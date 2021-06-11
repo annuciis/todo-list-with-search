a0f4154966a9f098e3493ab6389283b7aef3ee58
annuciis <58049115+annuciis@users.noreply.github.com>
2021-06-11 14:31:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+
+const generateTemplate = todo => {
+
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+    list.innerHTML += html;
+}
+
+addForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+    const todo = addForm.addField.value.trim();
+
+    if(todo != ""){
+    generateTemplate(todo);
+    addForm.addField.value = "";
+    }
+    
+})
+
+
+//delete todos
+list.addEventListener('click', e => {
+
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove(); //parent of icon is li item
+    }
+})
+
+//search todos
+
+const filterTodos = term => {
+
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('notInSearchList'));
+
+    Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('notInSearchList'));
+
+}
+
+search.addEventListener('keyup', () => {
+
+    let term = search.value.trim();
+    term = term.toLowerCase();
+    filterTodos(term);
+
+
+})
